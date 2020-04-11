aece1ce67c9c8a252a63ea6192a8eebcac262d6e
daffarafikanza <61929389+daffarafikanza@users.noreply.github.com>
2020-04-11 23:27:59 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+	{
+		static void Main(string[] args)
+		{
+			Person person = new Person("Muhammad Daffa", 19);
+			person.GetNameAndAge();
+
+			Teacher teacher = new Teacher("Rafikanza", 30, "191128", "Math");
+			teacher.GetNameAndAge();
+
+			Student student = new Student("Noor", 19, "2832", "muhammad.2826@students.amikom.ac.id");
+			student.GetNameAndAge();
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Teacher.cs
@@ -0,0 +1,13 @@
+﻿namespace LatihanInheritance
+{
+	public class Teacher : Person
+	{
+        public string TeacherId { get; set; }
+		public string Subject { get; set; }
+		public Teacher (string name, int age, string teacherid, string subject) : base(name, age)
+		{
+			this.TeacherId = teacherid;
+			this.Subject = subject;
+		}
+	}
+}
